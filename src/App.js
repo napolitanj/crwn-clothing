d8c65b693c5826557ae7599bf6f3933b7110d13b
JN <napolitanjoe@gmail.com>
2024-05-24 20:57:41 -0600

    Add foundation of navigation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
 import Home from './components/routes/home/home.component'
 import { Routes, Route, Outlet } from 'react-router-dom'
-
-const Navigation = () => {
-  return (
-    <div>
-      <div>
-        <h1>I am the navigation bar</h1>
-      </div>
-      <Outlet />
-    </div>
-  )
-}
+import Navigation from './components/routes/navigation/navigation.component'
 
 const Shop = () => {
   return <h1>I am the shop page</h1>;
