a2192684b3121add9bd1b46ccc0f6bc9ee8eb074
JN <napolitanjoe@gmail.com>
2024-05-31 10:27:32 -0600

    Initialize Firebase and add sign in component and link

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Home from './components/routes/home/home.component'
 import { Routes, Route, Outlet } from 'react-router-dom'
 import Navigation from './components/routes/navigation/navigation.component'
+import SignIn from './components/routes/sign-in/sign-in.component'
 
 const Shop = () => {
   return <h1>I am the shop page</h1>;
@@ -11,7 +12,8 @@ const App = () => {
     <Routes>
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route path='/shop' element={<Shop />} />
+        <Route path='shop' element={<Shop />} />
+        <Route path="signIn" element={<SignIn />} />
       </Route>
     </Routes>
   );
