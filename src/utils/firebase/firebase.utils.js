4d855604fcd537f776d5000ea70057eb2d95f184
JN <napolitanjoe@gmail.com>
2024-06-09 19:01:24 -0600

    Add sign up form

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth, 
+import { 
+  getAuth, 
   signInWithRedirect, 
   signInWithPopup, 
   GoogleAuthProvider 
@@ -22,22 +23,26 @@ const firebaseConfig = {
   
   const firebaseApp = initializeApp(firebaseConfig);
 
-  const provider = new GoogleAuthProvider();
-  provider.setCustomParameters({
+  const googleProvider = new GoogleAuthProvider();
+  
+  googleProvider.setCustomParameters({
     prompt: "select_account"
   });
 
   export const auth = getAuth();
-  export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+  export const signInWithGooglePopup = () => 
+    signInWithPopup(auth, googleProvider);
+  export const signInWithGoogleRedirect = () => 
+    signInWithRedirect(auth, googleProvider);
 
   export const db = getFirestore()
 
-  const creatUserDocumentFromAuth = async (userAuth) => {
+  export const createUserDocumentFromAuth = async (userAuth) => {
     const userDocRef = doc(db, 'users', userAuth.uid)
     const userSnapshot = await getDoc(userDocRef);
 
     if(!userSnapshot.exists()) {
-      const { dsiplayName, email} = userAuth;
+      const { displayName, email} = userAuth;
       const createdAt = new Date();
 
       try {
